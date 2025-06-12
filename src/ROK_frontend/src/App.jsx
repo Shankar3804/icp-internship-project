c6e462bfee6cb99edb8d08d0cc6b72688ee184ff
root <root@LAPTOP-U8SPMTBB.>
2025-06-12 06:27:42 +0000

    Initial commit.

---
+++ src/ROK_frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { ROK_backend } from 'declarations/ROK_backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    ROK_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
